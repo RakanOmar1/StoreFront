94d6e3eabb317095d5f05f23d34f8158832cdd05
Rakan Omar <RakanOmar1@users.noreply.github.com>
2026-09-12 20:48:22 +0300

    Configure Angular frontend for Vercel

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://mall7stars-d3c123d08509.herokuapp.com',
+  apiPrefix: '',
+  apiCapabilities: {
+    cart: true,
+    checkout: true
+  }
+}
